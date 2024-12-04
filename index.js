1d666eec5ccebe2345f811aa64ac97119fb29cb4
Henry-n-s <hnascimento257@gmail.com>
2024-12-04 17:44:38 -0300

    Update index.js
    versão final da calculadora

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-let nome = prompt("Digite seu nome.");
+let exp = ;
+let nome = ;
 
-let exp = prompt("Quanto de EXP você tem?");
 
-console.log("po")
+if (exp <= 1000) {
+console.log(nome, exp, "Nível ferro")
+} else if (exp => 1001 && exp <= 2000){
+    console.log(nome, exp, "Nível bronze")
+} else if (exp => 2001 && exp <= 5000){
+    console.log(nome, exp, "Nível prata/ouro")
+}else if (exp => 5001 && exp <= 8000){
+console.log(nome, exo, "Nível diamante")
+}else if (exp > 8001){
+    console.log (nome, exp, "Nível ascendente, parabéns!")
+};
 
 
